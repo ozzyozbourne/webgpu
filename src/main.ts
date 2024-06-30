b10c8516200c574a55c4ac550adbb36172a9ac61
ozzyozbourne <khanosaid726@gmail.com>
2024-06-30 15:07:32 -0500

    init webgpu

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import "./style.css";
+async function initWebGPU(): Promise<{
+  adapter: GPUAdapter;
+  device: GPUDevice;
+  context: GPUCanvasContext;
+  format: GPUTextureFormat;
+}> {
+  if (!navigator.gpu) throw new Error("Unable to init navigator");
+  const adapter = await navigator.gpu.requestAdapter();
+
+  if (!adapter) throw new Error("Unable to init adapter");
+  const device = await adapter.requestDevice({
+    requiredFeatures: ["texture-compression-bc"],
+    requiredLimits: {
+      maxStorageBufferBindingSize: adapter.limits.maxStorageBufferBindingSize,
+    },
+  });
+
+  adapter.features.forEach((v) => console.log(v));
+
+  console.log(adapter);
+
+  console.log(device);
+
+  const canvas = document.querySelector("canvas");
+  if (!canvas) throw new Error("Unable to init canvas");
+
+  const context = canvas.getContext("webgpu");
+  if (!context) throw new Error("Unable to init context");
+
+  const format = navigator.gpu.getPreferredCanvasFormat();
+
+  context.configure({
+    device,
+    format,
+  });
+
+  console.log(format);
+  return { adapter, device, context, format };
+}
+
+async function run() {
+  await initWebGPU();
+}
+
+run();
